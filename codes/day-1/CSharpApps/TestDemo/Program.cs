28ad215ad56e378faa67739a9d8104fbfff140c8
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-01 12:58:33 +0530

    day-1 final update

---
+++ codes/day-1/CSharpApps/TestDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello everyone.....");
+        }
+    }
+}
---
+++ codes/day-1/notepadapps/Program.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+	static void Main()
+	{
+		Console.WriteLine("Hello everyone.....");
+		Console.WriteLine("Press any key to terminate...");
+		Console.ReadLine();
+	}
+}
+
+//writeLine --> camel casing
+//WriteLine --> Pascal Casing
