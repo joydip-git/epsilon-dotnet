b9b05d8ea099e283d74384df52b87fccd9a928a8
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-02 11:42:47 +0530

    First VS App Created

---
+++ codes/day-2/CSharpFirstDemo/TestClient/UserInterface.cs
@@ -0,0 +1,21 @@
+﻿using MessageLibrary;
+using System;
+
+namespace TestClient
+{
+    class UserInterface
+    {
+        static void Main()
+        {
+            Messenger messenger = new Messenger();
+
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+
+            string message = messenger.SayWelCome(name);
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ codes/day-2/CSharpFirstDemo/TestLibrary/Messenger.cs
@@ -0,0 +1,10 @@
+﻿namespace MessageLibrary
+{
+    public class Messenger
+    {
+        public string SayWelCome(string name)
+        {
+            return "Welcome " + name + " in the world of C#";
+        }
+    }
+}
