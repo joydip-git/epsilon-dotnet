e25495ddb3c1b3289d329bd2517ff5229d528b8c
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-02 08:56:12 +0530

    code upload-2

--- codes/day-2/Executable.cs
+++ codes/day-2/Executable.cs
@@ -5,6 +5,10 @@ class Executable
 	static void Main()
 	{
 		Console.WriteLine("Hello World...");
+		
+		LibraryCls libraryCls = new LibraryCls();
+		string message = libraryCls.SayWelcome("Joydip");
+		Console.WriteLine(message);
 
 		Console.WriteLine("Press any key to terminate...");
 		Console.ReadLine();
