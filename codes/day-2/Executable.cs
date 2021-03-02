e2a6deb3684d408af628d296dd92554321f7351b
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-02 08:47:52 +0530

    code uploaded-1

---
+++ codes/day-2/Executable.cs
@@ -0,0 +1,12 @@
+using System; //System namespace
+
+class Executable
+{
+	static void Main()
+	{
+		Console.WriteLine("Hello World...");
+
+		Console.WriteLine("Press any key to terminate...");
+		Console.ReadLine();
+	}
+}
---
+++ codes/day-2/LibraryApp.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MyLibrary
+{
+	public class LibraryCls
+	{
+		public string SayWelcome(string name)
+		{
+			return "Welcome " + name;
+		}
+	}
+}
