4055986fcd82bb6cef9bca9d75293f79d03dca3d
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-05 11:36:58 +0530

    DAL updated

--- codes/day-5/HRMSAPP/HRMSAPP.DataAccessLayer/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace HRMSAPP.DataAccessLayer
-{
-    public class Class1
-    {
-    }
-}
---
+++ codes/day-5/HRMSAPP/HRMSAPP.DataAccessLayer/EmployeeDao.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using HRMSAPP.Entities;
+using HRMSAPP.Repository;
+
+namespace HRMSAPP.DataAccessLayer
+{
+    /// <summary>
+    /// Data Access Object containing methods to add, update, delete or return all the records
+    /// </summary>
+    public class EmployeeDao
+    {
+        #region Public Methods
+        /// <summary>
+        /// Method to insert an employee record
+        /// </summary>
+        /// <param name="employee">employee data</param>
+        /// <returns>
+        /// boolean, returns true if operation is successful and fale if operation is unsucessful 
+        /// </returns>
+        public bool Insert(Employee employee)
+        {
+            bool operationStatus = false;
+            //get the repository
+            Employee[] records = EmployeeRepository.Employees;
+
+            //check whether array is empty or not
+            bool empty = IsEmpty(records);
+            if (!empty)
+            {
+                //check the employee already exists or not
+                bool exists = Exists(employee.EmployeeId, records);
+                if (!exists)
+                {
+                    for (int i = 0; i < records.Length; i++)
+                    {
+                        if (records[i] == null)
+                        {
+                            records[i] = employee;
+                            operationStatus = true;
+                            break;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                records[0] = employee;
+            }
+
+            return operationStatus;
+        }
+
+        /// <summary>
+        /// Method to fetch all employees
+        /// </summary>
+        /// <returns>
+        /// returns array of employee records if not empty else a blank array if empty
+        /// </returns>
+        public Employee[] GetAllRecords()
+        {
+            //get the repository
+            Employee[] records = EmployeeRepository.Employees;
+
+            //check whether array is empty or not
+            bool empty = IsEmpty(records);
+            if (empty)
+            {
+                return new Employee[0];
+            }
+            else
+            {
+                return records;
+            }
+        }
+
+        /// <summary>
+        /// Method to return record of an employee given the employee id
+        /// </summary>
+        /// <param name="employeeId">id of an employee to fetch</param>
+        /// <returns>
+        /// returns employee instance if the record is found else null if not
+        /// </returns>
+        public Employee GetARecord(int employeeId)
+        {
+            //get the repository
+            Employee[] records = EmployeeRepository.Employees;
+
+            //check whether array is empty or not
+            bool empty = IsEmpty(records);
+
+            if (empty)
+            {
+                return null;
+            }
+            else
+            {
+                //check the employee already exists or not and also get the employee reference if exists
+                Employee found = null;
+                FetchEmployee(employeeId, records, ref found);
+                return found;
+                //bool exists = Exists(employeeId, records, ref found);
+                //if (exists)
+                //{
+                    //return FetchEmployee(employeeId, records);
+                    //return found;
+                //}
+                //else
+                //{
+                //    return null;
+                //}
+            }
+        }
+        #endregion
+
+        #region Helper Methods
+        private bool Exists(int employeeId, Employee[] employees)
+        {
+            bool exists = false;
+
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                {
+                    if (employee.EmployeeId == employeeId)
+                    {
+                        exists = true;
+                        break;
+                    }
+                }
+            }
+
+            return exists;
+        }
+
+        private bool IsEmpty(Employee[] employees)
+        {
+            bool isEmpty = true;
+            foreach (Employee employee in employees)
+            {
+                if (employee != null)
+                {
+                    isEmpty = false;
+                    break;
+                }
+            }
+            return isEmpty;
+        }
+
+        private void FetchEmployee(int employeeId, Employee[] employees, ref Employee found)
+        {
+            //Employee found = null;
+            foreach (Employee e in employees)
+            {
+                if (e != null)
+                {
+                    if (e.EmployeeId == employeeId)
+                    {
+                        found = e;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    #endregion
+}
