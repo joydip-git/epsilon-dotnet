be8021a20400bf93e16dd53aef403b3edd53bcab
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-05 13:55:15 +0530

    end of day-5 update

--- codes/day-5/HRMSAPP/HRMSAPP.UserInterface/Program.cs
+++ codes/day-5/HRMSAPP/HRMSAPP.UserInterface/Program.cs
@@ -1,12 +1,65 @@
 ﻿using System;
+using HRMSAPP.Entities;
+using HRMSAPP.BusinessLayer;
 
 namespace HRMSAPP.UserInterface
 {
     class Program
     {
-        static void Main(string[] args)
+        //EmployeeBo employeeBo;
+
+        static void ShowMenu()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("1. Add an Employee Record");
+            Console.WriteLine("2. Fetch an employee record");
+            Console.WriteLine("3. Fetch all records");
+        }
+        static int GetChoice()
+        {
+            Console.Write("\nEnter Choice: ");
+            return int.Parse(Console.ReadLine());
+        }
+        static Employee Create()
+        {
+            Employee employee = new Employee();
+
+            Console.Write("\nName: ");
+            employee.EmployeeName = Console.ReadLine();
+
+            Console.Write("Locaton: ");
+            employee.EmployeeLocation = Console.ReadLine();
+
+            Console.Write("Project Name: ");
+            employee.ProjectName = Console.ReadLine();
+
+            Console.Write("Basic Pay: ");
+            employee.EmployeeBasicPayemnt = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Da Pay: ");
+            employee.EmployeeDaPayemnt = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Hra Pay: ");
+            employee.EmployeeHraPayemnt = decimal.Parse(Console.ReadLine());
+
+            return employee;
+        }
+        static void Main()
+        {
+            EmployeeBo employeeBo = new EmployeeBo();
+            ShowMenu();
+            int choice = GetChoice();
+
+            switch (choice)
+            {
+                case 1:
+                    Employee employee = Create();
+                    bool addStatus = employeeBo.AddRecord(employee);
+                    Console.WriteLine($"{(addStatus ? "Added Successfully" : "Failed")}");
+                    break;
+
+                default:
+                    break;
+            }
         }
     }
 }
