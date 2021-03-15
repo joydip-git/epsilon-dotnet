70f6ea277693fe868df40cf3aa2dfa25a5dfcb38
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-15 10:10:31 +0530

    delegate implementation

---
+++ codes/day-11/DelegateApps/Implementation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Implementation
+{
+    class Program
+    {
+        //static 
+        static void Main()
+        {
+            List<int> numbers = new List<int> { 1, 3, 4, 2, 7, 5, 8, 0, 6, 9 };
+            //filter this list to produce another list containing only even numbers fron the original list
+        }
+    }
+}
