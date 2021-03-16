c68a61f14ad63bc7abf4efe26a7b214d935aa80d
siemens-c2c <joy_mon78@yahoo.co.in>
2021-03-16 10:27:11 +0530

    basic JS and HTML uploaded

--- codes/day-12/web-demo/index.js
+++ codes/day-12/web-demo/index.js
@@ -1,21 +1,75 @@
+var resultArray = [];
+
 function butonClicked() {
     //console.log('button clicked');
 
     //DOM APIs
-    var txtFirst =getElement('txtFirstValue');
+    var txtFirst = getElement('txtFirstValue');
     var txtSecond = getElement('txtSecondValue');
 
     var firstValue = getValue(txtFirst);
     var secondValue = getValue(txtSecond);
 
-    var result = add(firstValue, secondValue)
-    window.alert("result: " + result)
+    var radios = document.getElementsByName('calculationRadios');
+    var checkedRadio = null;
+    for (var index = 0; index < radios.length; index++) {
+        var radio = radios[index];
+        if (radio.checked) {
+            checkedRadio = radio;
+            break;
+        }
+    }
+
+    // x === y, !==, x == y, !=
+    if (checkedRadio != null) {
+        var result = calculate(checkedRadio, firstValue, secondValue);
+        resultArray.push(result);
+        window.alert("result: " + result)
+    }
+    else {
+        window.alert('make a proper choice');
+    }
 }
-function getElement(elementId){
+function showAllClicked() {
+    if (resultArray.length > 0) {
+        for (var index = 0; index < resultArray.length; index++) {
+            var result = resultArray[index];
+            window.alert(result);
+        }
+    }
+}
+
+function calculate(radio, first, second) {
+    var result = 0;
+    switch (radio.value) {
+        case 'Add':
+            result = first + second;
+            break;
+
+        case 'Subtract':
+            result = first - second;
+            break;
+
+        case 'Multiply':
+            result = first * second;
+            break;
+
+        case 'Divide':
+            result = first / second;
+            break;
+
+        default:
+            break;
+    }
+    return `${radio.value} result is ${result}`;
+}
+
+
+function getElement(elementId) {
     return document.getElementById(elementId);
 }
 
-function getValue(txtBox){
+function getValue(txtBox) {
     return parseInt(txtBox.value);
 }
 
